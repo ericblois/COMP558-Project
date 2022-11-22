321bfc9b02cd46ecc30ee91a3f79fdfaa4fdea3b
Alex Rojco <alexrojco@gmail.com>
2022-11-22 11:51:20 -0500

    using mediapipe to detect hands

--- Camera.py
+++ Camera.py
@@ -1,30 +1,46 @@
 import cv2
 import time
 import numpy as np
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 def main():
-    # define a video capture object
-    vid = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    with mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
 
-    while True:
+                # To improve performance, optionally mark the image as not writeable to
+                # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
 
-        # Capture the video frame
-        ret, frame = vid.read()
-
-        # Flip frame horizontally
-        frame = cv2.flip(frame, 1)
-
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-
-        # Quit on press 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # After the loop release the vid object
-    vid.release()
-    cv2.destroyAllWindows()
-
+            # Draw the hand annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
 
 if __name__ == "__main__":
     main()
